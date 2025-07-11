a27a590cb9b754fe28bce97312883314892172db
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2025-07-11 13:09:51 -0700

    Start moving functions into their own module

---
+++ organize_directory/organize_directory.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+
+def move_file(file: Path, target_dir: Path) -> None:
+    """Move `file` into `target_dir`, creating `target_dir` as necessary."""
+
+    if not target_dir.exists():
+        target_dir.mkdir()
+    file.rename(target_dir / file.name)
+
+
+def move_image(image_file: Path, target_dir: Path) -> None:
+    """Move an image and its sidecar file to `target_dir`."""
+
+    move_file(image_file, target_dir)
+
+    sidecar_file = image_file.with_suffix(".xmp")
+    try:
+        sidecar_file.rename(target_dir / sidecar_file.name)
+    except FileNotFoundError:
+        pass  # Do nothing if a sidecar file does not exist
