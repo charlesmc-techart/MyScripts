e867c999ac3efe7f82198cee6086f142fabd94f9
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2025-07-14 18:18:08 -0700

    Make generic

--- process_screenshots/main.sh
+++ process_screenshots/main.sh
@@ -1,52 +1,56 @@
 #!/usr/bin/env zsh -f
-# A Folder Action script for renaming screenshots and adding certain metadata
+# A script for renaming screenshots and adding certain metadata
 
-readonly SCREENSHOTS_DIR=~/MyFiles/Pictures/Screenshots/.tmp
-cd ~SCREENSHOTS_DIR || exit $?
-
-setopt EXTENDED_GLOB
-readonly FILENAME_PATTERN='*2<-1><-9><-9>-<-1><-9>-<-3><-9>*<-2><-9>.<-5><-9>.<-5><-9>*.*(.)'
-readonly DIRNAME=${0:A:h:t}
-readonly PIPE=../$DIRNAME
-readonly TAG_FILES_DIR=~/.config/exiftool
-
-################################################################################
-
-if [[ ! ./~FILENAME_PATTERN ]] 2>/dev/null; then
-    echo "No screenshots to process" >&2
-    exit 2
-elif [[ -p $PIPE ]]; then
-    echo "Pipe '$PIPE' exists, so the folder action is already in progress" >&2
+if (( $# < 1 )); then
+    echo "No directory to process" >&2
     exit 1
 fi
-# Taking multiple screenshots in succession causes the Folder Action to trigger
-# the same amount of times. Checking for this temporary pipe in the `elif`
-# statement above ensures that only the first instance of the Folder Action
-# executes the rest of the script body
-mkfifo $PIPE && trap 'rm $PIPE' EXIT
 
-readonly search_str='Model Name:'
-readonly hardware=$(system_profiler SPHardwareDataType | grep $search_str | sed -E "s/.*${search_str} ?//")
+cd $1 2>/dev/null
+if (( $? > 0 )); then
+    echo "Not a directory: $1" >&2
+    exit 2
+fi
+
+readonly orig_filename_pattern='*2<-1><-9><-9>-<-1><-9>-<-3><-9>*<-2><-9>.<-5><-9>.<-5><-9>*.*(.)'
+if [[ ! ./~orig_filename_pattern ]] 2>/dev/null; then
+    echo "No screenshots to process" >&2
+    exit 3
+fi
+
+# Parse tag files
+shift
+declare -a tag_files
+for arg in $@; do
+    [[ ${arg:e} != 'args' ]] && continue
+
+    tag_files+="-@ $arg"
+    shift
+done
+
+# PERL string replacement patterns that will be used by ExifTool
+readonly re='^.+?(2[0-1])(\d{2})-([0-1]\d)-([0-3]\d).+([0-2]\d)\.([0-5]\d)\.([0-5]\d)(\s\(\d+?\))?\..+?$'
+readonly orig_str_pattern="Filename;s/${re}"
+readonly new_filename_pattern="\${${orig_str_pattern}/\$2\$3\$4_\$5\$6\$7\$8.%e/}"
+readonly new_datetime_pattern="\${${orig_str_pattern}/\$1\$2-\$3-\$4T\$5:\$6:\$7${timezone}/}"
 
 readonly timezone=$(date +%z)
 
-# PERL string replacement patterns that will be used by ExifTool
-readonly re='^.+?(2[0-1])(\d{2})-([0-1]\d)-([0-3]\d).+([0-2]\d)\.([0-5]\d)\.([0-5]\d)(\W*?\d+?)?\..+?$'
-readonly orig_str_pattern="Filename;s/${re}"
-readonly filename_pattern="\${${orig_str_pattern}/\$2\$3\$4_\$5\$6\$7\$8.%e/}"
-readonly datetime_pattern="\${${orig_str_pattern}/\$1\$2-\$3-\$4T\$5:\$6:\$7${timezone}/}"
+readonly search_str='Model Name:'
+readonly hardware=$(system_profiler SPHardwareDataType | grep $search_str | sed -E "s/ *${search_str} ?//")
 
-/opt/homebrew/bin/exiftool -P -v -struct      $~FILENAME_PATTERN\
-     -directory=../                           "-Filename<$filename_pattern"\
-    "-AllDates<$datetime_pattern"             "-OffsetTime*=$timezone"\
+/opt/homebrew/bin/exiftool -P -struct         $=tag_files\
+    $~orig_filename_pattern                   "-Filename<$new_filename_pattern"\
+    "-AllDates<$new_datetime_pattern"         "-OffsetTime*=$timezone"\
     '-MaxAvailHeight<ImageHeight'             '-MaxAvailWidth<ImageWidth'\
     '-RawFileName<FileName'                   '-PreservedFileName<FileName'\
-    "-Software=$(sw_vers --productVersion)"   "-Model=$hardware"\
-     -@ ~TAG_FILES_DIR/charlesmc.args          -@ ~TAG_FILES_DIR/screenshot.args
+    "-Software=$(sw_vers --productVersion)"   "-Model=$hardware"
 
-if (( $? == 0 )); then
-    # Back up the originals in an Apple Archive before deleting them
-    aa archive -o "${SCREENSHOTS_DIR:h}/originals_$(date +%y%m%d_%H%M%S).aar"\
-        -d . -a lzma -exclude-name .DS_Store\
-        && rm ./*(.)
+if (( $? == 0)); then
+    tmp_dir=$(mktemp -d -t $0:h:t)
+    mv $~orig_filename_pattern ~tmp_dir
+
+    aa archive -o "Screenshots_$(date +%y%m%d_%H%M%S).aar" -d ~tmp_dir -a lzma\
+        -exclude-name .DS_Store\
+        && rm -rf ~tmp_dir
 fi
